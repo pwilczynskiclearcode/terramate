273d92d93f1169fdc6d9106f3a8b57aee7738a88
i4k <t.nateldemoura@gmail.com>
2023-05-05 21:31:54 +0100

    chore: check against base branch

--- makefiles/common.mk
+++ makefiles/common.mk
@@ -82,7 +82,7 @@ bench/check: allocdelta="+20%"
 bench/check: timedelta="+20%"
 bench/check: name=github.com/mineiros-io/terramate
 bench/check: pkg=./...
-bench/check: old=main
+bench/check: old=i4k-benchmark-readdir
 bench/check: new?=$(shell git rev-parse HEAD)
 bench/check:
 	@$(BENCH_CHECK) -mod $(name) -pkg $(pkg) -go-test-flags "-benchmem,-count=20,-run=Bench" \
